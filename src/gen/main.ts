7dafe676a4a1e60a4a2ee699c6ac58eb941d05a1
Thibault Le Ouay Ducasse <thibaultleouay@gmail.com>
2026-01-23 12:33:37 +0100

    init sdk

---
+++ src/gen/main.ts
@@ -0,0 +1,13 @@
+import { createClient } from "@connectrpc/connect";
+import { createConnectTransport } from "@connectrpc/connect-node";
+import { MonitorService } from "./openstatus/monitor/v1/service_pb.ts";
+
+
+const transport = createConnectTransport({
+  baseUrl: process.env.OPENSTATUS_API_URL ?? "https://api.openstatus.dev",
+  httpVersion: "2",
+});
+
+export const openstatus = {
+  monitor: { v1: { MonitorService: createClient(MonitorService, transport) } },
+};
