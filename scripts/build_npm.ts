ed7ae077afa79a14ce96471a19cc6a014d4fbf5b
Thibault Le Ouay Ducasse <thibaultleouay@gmail.com>
2026-01-26 21:21:51 +0100

    improve sdk

---
+++ scripts/build_npm.ts
@@ -0,0 +1,31 @@
+import { build, emptyDir } from "@deno/dnt";
+
+await emptyDir("./npm");
+
+await build({
+  entryPoints: ["./src/mod.ts"],
+  outDir: "./npm",
+  shims: {
+    // see JS docs for overview and more options
+    deno: true,
+  },
+  package: {
+    // package.json properties
+    name: "@openstatus/sdk-node",
+    version: Deno.args[0],
+    description: "SDK for openstatus.",
+    license: "MIT",
+    repository: {
+      type: "git",
+      url: "git+https://github.com/openstatushq/sdk-node.git",
+    },
+    bugs: {
+      url: "https://github.com/openstatushq/sdk-nod/issues",
+    },
+  },
+  postBuild() {
+    // steps to run after building and before running the tests
+    Deno.copyFileSync("LICENSE", "npm/LICENSE");
+    Deno.copyFileSync("README.md", "npm/README.md");
+  },
+});
